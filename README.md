17049e3f59e8da183d82cea3e1650601b168cbec
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-02-14 15:59:11 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Multilayer Perceptron project of 42
+
+## Notes
+
+Dans le reseau de neurones, il faut que le premier layer (l'input layer) ait un neurone par feature de mes donnees d'entree. 
+
+et ensuite il n'applique pas de poids autre que un neurone par feature ==> On peut utiliser une matrice avec des 1 et des 0. 
+
+et on applique la fonction d'activation. 
+
+
+entre un calque et un autre, la taille de la matrice W va dependre du nombre de neurone dans le calque d'avant et du nombre de neurone dans le calque courant. 
+
+Dans le cas Calque 1 avec 4 neurones => calque 2 avec 3 neurones.
+
+N1_1       
+N1_2        N2_1
+N1_2        N2_2
+N1_4        N2_3
+
+
+Par exemple le resultat de ce qui sort de N2_1 est :
+function_activation(W.a_in + b)
+soit avec a_in de la forme [a_1, a_2, a_3, a_4]
+et w = [w2_1_1, W2_1_2, W2_1_3, W2_1_4]
+
+f_act(W2_1_1*a_1 + W2_1_2*a_2 + W_2_1_3*a_3 + W_2_1_4*a_4 + b)
+
+
--- class_declarations.py
+++ class_declarations.py
@@ -3,26 +3,101 @@ import numpy as np
 from typing import Any
 
 
-
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
 
 def relu(z):
     return np.max(0, z)
 
+
 def linear(z):
     return z
+
 
 def softmax(z):
     return
 
+
+def constantWeightInit(input_units, output_units, value=1):
+    return np.ones(input_units, output_units) * value
+
+
+def uniformWeightInit(input_units, output_units, low=-0.05, high=0.05):
+    return np.random.uniform(low, high, size=(input_units, output_units))
+
+
+def normalWeightInit(input_units, output_units, mean=0.0, std=0.05):
+    return np.random.normal(mean, std, size=(input_units, output_units))
+
+
+def leCunUniformWeightInit(in_units, out_units):
+    limit = np.sqrt(3 / float(in_units))
+    return np.random.uniform(low=-limit, high=limit, size=(in_units, out_units))
+
+
+def leCunNormalWeightInit(in_units, out_units):
+    limit = np.sqrt(1 / float(in_units))
+    return np.random.normal(0.0, limit, size=(in_units, out_units))
+    
+
+def xavierUniformWeightInit(in_units, out_units):
+    limit = np.sqrt(6 / float(in_units + out_units))
+    return np.random.uniform(-limit, limit, size=(in_units, out_units))
+    
+
+def xavierNormalWeightInit(in_units, out_units):
+    limit = np.sqrt(2 / float(in_units + out_units))
+    return np.random.normal(0.0, limit, size=(in_units, out_units))
+
+
+def heUniformWeightInit(in_units, out_units):
+    limit = np.sqrt(6 / float(in_units))
+    return np.random.uniform(low=-limit, high=limit, size=(in_units, out_units))
+
+
+def heNormalWeightInit(in_units, out_units):
+    limit = np.sqrt(2 / float(in_units))
+    return np.random.normal(0.0, limit, size=(in_units, out_units))
+
+
+# Besoin de gerer weights_initializer
 class DenseLayer(ABC):
-    """Class for the Dense Layer"""
-    def __init__(self, nb_Node:int, activation:str, node_size = 1, W = None, b = None, layerName = "Dense") -> None:
+    """Class for the Dense Layer:
+    nb_Node:int
+    input_shape: int, nb of nodes of the previous layer
+    activation: str, type of the activation function used
+    W: array (input_shape, nb_Node), can be given as argument to load existing values.
+    b: array(nb_Node), the bias used
+    layername: str, a name so it's easier to organise the neural network
+    weights_initializer:str, the method used for weights initialisation
+    """
+    def __init__(self, nb_Node:int, activation = "relu", W = None, b = None,
+                 layerName = "Dense", weights_initializer = 'heUniform') -> None:
         """My Dense Layer constructor"""
         super().__init__()
         self.nb_Node = nb_Node
         self.layerName = layerName
+        self.input_shape = nb_Node
+        self.input_layer = True
+        self.setActivation(activation)
+        self.setWeightInit(weights_initializer)
+        if W is not None:
+            self.input_shape = len(W)
+            self.W = W
+        else:
+            self.W = np.random.rand(self.input_shape, nb_Node)
+        if b is not None:
+            self.b = b
+        else:
+            self.b = np.random.rand(nb_Node)
+        
+    def propag(self, a_in):
+        z = self.W.transpose() @ a_in + self.b
+        a_out = self.activation(z)
+        return a_out
+    
+    def setActivation(self, activation = "relu"):
         match activation:
             case "sigmoid":
                 self.activation = sigmoid
@@ -34,21 +109,36 @@ class DenseLayer(ABC):
                 self.activation = softmax
             case _:
                 raise ValueError("Unknowed activation function")
-        if W is not None:
-            self.node_size = len(W)
-            self.W = W
-        else:
-            self.node_size = node_size
-            self.W = np.random.rand(node_size, nb_Node)
-        if b is not None:
-            self.b = b
-        else:
-            self.b = np.random.rand(nb_Node)
-        
-    def propag(self, a_in):
-        z = self.W.transpose() @ a_in + self.b
-        a_out = self.activation(z)
-        return a_out
+    
+    def setWeightInit(self, weights_initializer = 'heUniform'):
+        match weights_initializer:
+            case "constant":
+                self.weights_initializer = constantWeightInit
+            case "uniform":
+                self.weights_initializer = uniformWeightInit
+            case "normal":
+                self.weights_initializer = normalWeightInit
+            case "leCunUniform":
+                self.weights_initializer = leCunUniformWeightInit
+            case "leCunNormal":
+                self.weights_initializer = leCunNormalWeightInit
+            case "xavierUniform":
+                self.weights_initializer = xavierUniformWeightInit
+            case "xavierNormal":
+                self.weights_initializer = xavierNormalWeightInit
+            case "heUniform":
+                self.weights_initializer = heUniformWeightInit
+            case "heNormal":
+                self.weights_initializer = heNormalWeightInit
+            case _:
+                raise ValueError("Unknowed weights_initializer function")
+
+    
+    def setInputShape(self, input_shape):
+        self.input_shape = input_shape
+        # Uniquement si changement de la taille de W besoin de le redefinir
+        if self.input_shape != len(self.W):
+            self.W = np.random.rand(self.input_shape, self.nb_Node)
     
     def printLayer(self) -> None:
         print("Layer Name:",self.layerName)
@@ -61,14 +151,15 @@ class DenseLayer(ABC):
 
 class MySequencial(ABC):
     """Class for the Model"""
-    def __init__(self, *args: Any) -> None:
+    def __init__(self, layers) -> None:
         """My Model constructor"""
         super().__init__()
         self.layers = []
-        for arg in args:
-            if not isinstance(arg, DenseLayer):
+        for layer in layers:
+            if not isinstance(layer, DenseLayer):
                 raise ValueError("MySequencial needs DenseLayer as argument")
-            self.layers.append(arg)
+            # Besoin de modifier le layer en fonction de ce qu'on a deja recu.
+            self.layers.append(layer)
     
     def summary(self) -> None:
         for i in range(len(self.layers)):
